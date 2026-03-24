d83daec9313be778a4f763a87e1c60feb25f68c4
PensiveEagle <halljf96@gmail.com>
2026-03-24 12:11:10 +0000

    Setup basic API functionality

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 
-app = Flask( "Website" )
+app = Flask( __name__ )
 
-@app.route( "/home" )
+@app.route( "/" )
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route( "/about" )
-def about():
-    return render_template("about.html")
+@app.route( "/api/v1/<station_id>/<date>" )
+def about(station_id, date):
+    return {"station": station_id,
+            "date": date,
+            "temperature": "holding"}
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)

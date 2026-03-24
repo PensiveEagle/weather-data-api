f8628a2747c4eef6fc2113279b0cb1312b2ba592
PensiveEagle <halljf96@gmail.com>
2026-03-24 17:09:46 +0000

    Added yearly function
    Also ensured all date output was formatted

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pandas as pd
 import json
+from dateutil.parser import parse as date_parse
 
 def extract_data( data_filepath: str, skip_rows: int = 20 ) -> pd.DataFrame:
     df = pd.read_csv( data_filepath, skiprows = skip_rows )
@@ -12,6 +13,12 @@ def filter_dataframe_on_date( dataframe: pd.DataFrame, filter_date: datetime ) -
     dataframe = dataframe[dataframe["DATE"] == filter_date]
     return dataframe
 
+def filter_dataframe_on_year( dataframe: pd.DataFrame, filter_year: int ) -> pd.DataFrame:
+    dataframe["DATE"] = dataframe["DATE"].astype(str)
+    dataframe = dataframe[dataframe["DATE"].str.startswith(str(filter_year))]
+    return dataframe
+
 def dataframe_to_json( dataframe: pd.DataFrame ) -> dict:
+    dataframe["DATE"] = pd.to_datetime( dataframe["DATE"], format = "ISO8601" )
     json_string = dataframe.to_json( orient = "table", index = False )
     return json.loads( json_string )

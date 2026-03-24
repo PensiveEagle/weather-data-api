8069e2098f09b9504a68ffa19df834eb5dc63436
PensiveEagle <halljf96@gmail.com>
2026-03-24 15:37:57 +0000

    Added funtionality for getting station data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from functions import extract_data, filter_dataframe_on_date, dataframe_to_json
+from dateutil.parser import parse as date_parse
 
 app = Flask( __name__ )
 
@@ -6,11 +8,17 @@ app = Flask( __name__ )
 def home():
     return render_template("home.html")
 
+@app.route( "/api/v1/<station_id>" )
+def all_station_data(station_id):
+    station_identifier = station_id.zfill( 6 )
+    return dataframe_to_json( extract_data( f"./data/TG_STAID{station_identifier}.txt") )
+
 @app.route( "/api/v1/<station_id>/<date>" )
-def about(station_id, date):
-    return {"station": station_id,
-            "date": date,
-            "temperature": "holding"}
+def station_data(station_id, date):
+    filter_date = date_parse( date )
+    station_identifier = station_id.zfill( 6 )
+    return dataframe_to_json( filter_dataframe_on_date( extract_data( f"./data/TG_STAID{station_identifier}.txt"), filter_date ) )
+
 
 if __name__ == "__main__":
     app.run(debug=True, port = 5050)
